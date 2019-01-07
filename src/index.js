00f25433f00a82e036e32c44ba3be75188ac217f
krav-ets <krav-ets@yandex.ru>
2019-01-07 21:30:09 +0300

    add import jquery-ujs

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import 'jquery/dist/jquery.slim.min';
 import 'popper.js/dist/popper.min';
 import 'bootstrap/dist/js/bootstrap.min';
+import 'jquery-ujs/src/rails';
 import 'bootstrap/dist/css/bootstrap.min.css';
